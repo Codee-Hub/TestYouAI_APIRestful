25268b0e26686508f5903da2eea12843a552aca3
CauZy-Goes <cauafariasdev@gmail.com>
2025-07-25 20:03:50 -0300

    Inicialized Project

---
+++ src/main/java/github/devhub/testyouai/TestYouAiApplication.java
@@ -0,0 +1,13 @@
+package github.devhub.testyouai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestYouAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestYouAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/devhub/testyouai/TestYouAiApplicationTests.java
@@ -0,0 +1,13 @@
+package github.devhub.testyouai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestYouAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
