c316cc28fc918198c1cf11ba184066d35ec8dc70
CauZy-Goes <cauafariasdev@gmail.com>
2025-07-28 12:16:34 -0300

    feat: create Question entity

--- src/main/java/github/devhub/testyouai/domain/model/Question.java
+++ src/main/java/github/devhub/testyouai/domain/model/Question.java
@@ -4,13 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Test {
-    private String dificuldade;
-    private Integer numeroDePerguntas;
-    private List<Question> perguntasList;
+public class Question {
+    private String text;
+    private String justification;
+    private Boolean isCorrect;
 }
