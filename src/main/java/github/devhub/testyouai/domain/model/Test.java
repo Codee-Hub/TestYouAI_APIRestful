98bcb4ff76fb8edfe68cd8c6e5464182542524bb
cauzy-goes <cauafariasdev@gmail.com>
2025-08-02 10:32:37 -0300

    feat: mapped test

--- src/main/java/github/devhub/testyouai/domain/model/Test.java
+++ src/main/java/github/devhub/testyouai/domain/model/Test.java
@@ -1,5 +1,7 @@
 package github.devhub.testyouai.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +11,24 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Test {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String theme;
     private String level;
+
+    @Column(name = "number_of_questions")
     private Integer numberOfQuestions;
+
+    @Column(name = "question_list")
+    @OneToMany(mappedBy = "id")
     private List<Question> questionList;
 
+    @JsonIgnore
+    @ManyToOne
     private User user;
 }
