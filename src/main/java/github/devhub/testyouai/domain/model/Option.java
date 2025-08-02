9f74d5211cccd7043025acff94463edc53a7f084
cauzy-goes <cauafariasdev@gmail.com>
2025-08-02 10:42:18 -0300

    feat: mapped Option

--- src/main/java/github/devhub/testyouai/domain/model/Option.java
+++ src/main/java/github/devhub/testyouai/domain/model/Option.java
@@ -1,15 +1,34 @@
 package github.devhub.testyouai.domain.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Option {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "text_option")
     private String textOption;
+
+    @Column(name = "Justification")
     private String justification;
+
+    @Column(name = "was_select")
     private Boolean wasSelect = false;
+
+    @Column(name = "is_correct")
     private Boolean isCorrect;
+
+    @ManyToOne
+    private Question question;
 }
