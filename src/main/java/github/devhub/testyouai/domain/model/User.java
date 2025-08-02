73937a8fad98a7503c6e67df038bd313c632a084
cauzy-goes <cauafariasdev@gmail.com>
2025-08-02 10:28:14 -0300

    feat: mapped user entity

--- src/main/java/github/devhub/testyouai/domain/model/User.java
+++ src/main/java/github/devhub/testyouai/domain/model/User.java
@@ -1,5 +1,6 @@
 package github.devhub.testyouai.domain.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,12 +10,22 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String email;
+
     private String password;
+
     private String nome;
+
+    @Column(name = "phone_number")
     private String phoneNumber;
 
+    @OneToMany(mappedBy = "id")
     private List<Test> testList;
 }
