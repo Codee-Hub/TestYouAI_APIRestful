699e15037bd3806ab3de0fcc08120bdd23cb933a
CauZy-Goes <cauafariasdev@gmail.com>
2025-08-09 16:23:33 -0300

    Remover chave da API

---
+++ src/main/java/github/devhub/testyouai/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package github.devhub.testyouai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("TestYouAI API").version("v1.0"))
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("basicAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("basic")
+                                .description("Autenticação Básica")
+                        )
+                );
+    }
+}
+
--- src/main/java/github/devhub/testyouai/security/SecurityConfig.java
+++ src/main/java/github/devhub/testyouai/security/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                                 "/v3/api-docs",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
-                                "/swagger-ui.html"
+                                "/swagger-ui.html/**"
                         ).permitAll()
                         .requestMatchers("/user/**").hasRole("ADMIN")
                         .requestMatchers("/api/tests/**").permitAll()
