3bf277d8ea487b1eacb390bf77a67f2bd4660f2c
CauZy-Goes <cauafariasdev@gmail.com>
2025-08-13 17:40:11 -0300

    feat: create question and option dto

---
+++ src/main/java/github/devhub/testyouai/adapter/in/dto/OptionResponseDTO.java
@@ -0,0 +1,20 @@
+package github.devhub.testyouai.adapter.in.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import github.devhub.testyouai.domain.model.Question;
+import jakarta.persistence.*;
+
+public record OptionResponseDTO(
+         Long id,
+
+         String textOption,
+
+         String justification,
+
+         Boolean wasSelect,
+
+         Boolean isCorrect,
+
+         QuestionResponseDTO question
+) {
+}
---
+++ src/main/java/github/devhub/testyouai/adapter/in/dto/QuestionResponseDTO.java
@@ -0,0 +1,17 @@
+package github.devhub.testyouai.adapter.in.dto;
+
+import github.devhub.testyouai.domain.model.Option;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+public record QuestionResponseDTO(
+        Long id,
+
+        String textQuestion,
+
+        List<OptionResponseDTO> optionList,
+
+        TestResponseBasicDTO test
+) {
+}
