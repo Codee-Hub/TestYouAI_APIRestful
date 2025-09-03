956c6a5615a27b8a8694aa33313b7da5fb57f4aa
CauZy-Goes <cauafariasdev@gmail.com>
2025-09-03 10:37:28 -0300

    refactor: user dto class to record

--- src/main/java/github/devhub/testyouai/adapter/in/dto/UserRequestDTO.java
+++ src/main/java/github/devhub/testyouai/adapter/in/dto/UserRequestDTO.java
@@ -1,31 +1,14 @@
 package github.devhub.testyouai.adapter.in.dto;
 
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import jakarta.validation.constraints.*;
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.NoArgsConstructor;
-
-
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserRequestDTO {
-    @NotNull
-    @NotBlank
-    private String name;
-
-    @Email
-    @NotNull
-    @NotBlank
-    private String email;
-
-    private String password;
-
-    private String phoneNumber;
-}
-
+public record UserRequestDTO(
+        @NotBlank @Size(min = 3, max = 20)
+        String name,
+        @Email @NotBlank
+        String email,
+        @NotBlank @Size(min = 3, max = 20)
+        String password,
+        @NotBlank
+        String phoneNumber
+) {}
--- src/main/java/github/devhub/testyouai/adapter/in/dto/UserResponseDTO.java
+++ src/main/java/github/devhub/testyouai/adapter/in/dto/UserResponseDTO.java
@@ -3,26 +3,10 @@ package github.devhub.testyouai.adapter.in.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class UserResponseDTO {
-    @NotNull
-    @NotBlank
-    private String name;
-
-    @Email
-    @NotNull
-    @NotBlank
-    private String email;
-
-    private String password;
-
-    private String phoneNumber;
-}
-
+public record UserResponseDTO(
+        String name,
+        String email,
+        String password,
+        String phoneNumber
+) {}
