aece8fa5c16a2b6fa3ec92798e7c0fe0532c200d
CauZy-Goes <cauafariasdev@gmail.com>
2025-09-08 10:27:32 -0300

    feat: add roles in response dto

--- src/main/java/github/devhub/testyouai/adapter/in/dto/UserResponseDTO.java
+++ src/main/java/github/devhub/testyouai/adapter/in/dto/UserResponseDTO.java
@@ -1,13 +1,17 @@
 package github.devhub.testyouai.adapter.in.dto;
 
+import github.devhub.testyouai.domain.model.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+
+import java.util.Set;
 
 public record UserResponseDTO(
         Long id,
         String name,
         String email,
         String password,
-        String phoneNumber
+        String phoneNumber,
+        Set<Role> roles
 ) {}
